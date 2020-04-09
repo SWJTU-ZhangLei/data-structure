ae3e8b628cf76f8762a8e97649c355af6825e6b0
root <SWJTU-ZhangLei>
2020-04-09 15:39:13 +0800

    My first commit.

---
+++ c/slinklist/slinklist.h
@@ -0,0 +1,11 @@
+#ifndef _SLINKLIST_H_
+#define _SLINKLIST_H_
+
+define SLINKLIST_MAX_SIZE 1000
+
+typedef struct {
+    int cur;
+    char data;
+} Component, SLinkList[SLINKLIST_MAX_SIZE];
+
+#endif //_SLINKLIST_H_
