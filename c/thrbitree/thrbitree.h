0aeff0210437530c60c1162f5f56a24df21f0d52
root <SWJTU-ZhangLei>
2020-04-20 18:22:36 +0800

    Add thrbitree.

---
+++ c/thrbitree/thrbitree.h
@@ -0,0 +1,15 @@
+#ifndef _THRBITREE_H_
+
+#define _THRBITREE_H_
+
+typdef int TElemType;
+
+typedef enum PointerTag { Link, Thread }; // Link 0, Thread 1
+
+typdef struct ThrBiTreeNode {
+    TElemType data;
+    ThrBiTreeNode *lchild, *rchild;    
+    PointerTag ltag, rtag;
+} ThrBiTreeNode, *ThrBiTree;
+
+#endif //_THRBITREE_H_
